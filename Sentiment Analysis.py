80e10c01f56eb78ac8ca462e264fb9b653410d40
Rahul Kushwaha <103225615+rahulk1818@users.noreply.github.com>
2023-05-27 23:58:48 +0530

    Add files via upload

---
+++ Sentiment Analysis.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM,Dense, Dropout, SpatialDropout1D
+from tensorflow.keras.layers import Embedding
+
+df = pd.read_csv("./Tweets.csv")
+df.head()
